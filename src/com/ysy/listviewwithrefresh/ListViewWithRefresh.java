3c355872e9d2a7e47317fb0a2faf446ae95a5149
Yin Shengyi <945396115@qq.com>
2015-03-07 10:02:58 +0800

    The second edition

--- src/com/ysy/listviewwithrefresh/ListViewWithRefresh.java
+++ src/com/ysy/listviewwithrefresh/ListViewWithRefresh.java
@@ -3,7 +3,6 @@ package com.ysy.listviewwithrefresh;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
